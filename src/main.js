c9f612d3d1f168fea694b71cc8c4a3506623e602
Erik Verstegen <erik@flowingbytes.nl>
2018-12-14 10:36:03 +0100

    Automatically inject the plugin when the script is loaded

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import VueCustomElement from 'vue-custom-element'
 import BugTracker from './components/BugTracker.vue'
 import Vue from 'vue'
 
-Vue.use(VueCustomElement)
+// Inject the plugin.
+var $bugtracker = document.createElement('bug-tracker')
+$bugtracker.setAttribute('unresolved', '5bf52bcc81455a2f35ba8e11')
 
+document.body.appendChild($bugtracker)
+
+Vue.use(VueCustomElement)
 Vue.customElement('bug-tracker', BugTracker)
